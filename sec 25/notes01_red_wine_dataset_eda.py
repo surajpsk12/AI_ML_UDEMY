8cd97a600cdb71a7be9fb92c0a640f6148dfe886
surajvansh12 <sk658139@gmail.com>
2026-08-17 22:52:31 +0530

    Add initial EDA script for flight price dataset with data preprocessing notes

---
+++ sec 25/notes01_red_wine_dataset_eda.py
@@ -0,0 +1,19 @@
+"""
+
+topic : red wine dataset eda 
+
+defination 
+
+ex 
+
+real life case study
+
+use in ai ml 
+
+real problem solved code 
+
+conclusion 
+
+
+
+"""
---
+++ sec 25/notes02_flight_price_dataset_eda.py
@@ -0,0 +1,23 @@
+"""
+
+topic 
+
+defination 
+
+ex 
+
+real life case study
+
+use in ai ml 
+
+real problem solved code 
+
+conclusion 
+
+
+isme data diya hai jo bhi usko numerical value me lana hia jo model understand kar sake like date 19/02/2026 isko day month year me break karo new column banake day = 19 , month = 02 year = 2026 , and time me bhi minute alag hour alag . 
+same with agar koi string hia  to oneHotencoder se change karo aur value me leke aao . means raw data se meaning full data banana fir model ko send karna 
+
+
+"""
+
